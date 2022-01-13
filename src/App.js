9c49d1c454f378aafd3ca499c4aa8f7c15e2f244
anuannasharon <anu@zeksta.com>
2022-01-13 07:05:26 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Board from './components/Board';
+import React, {useState, useCallback} from 'react';
+import './App.css'
 function App() {
+
+  const [size, setSize] = useState(0);
+  const [userMaze, setUsrMaze] = useState('X')
+  const [systemMaze, setSystemMaze] = useState('O')
+  
+  const setMazeSize = (size) =>{
+    setSize(Number(size))
+  };
+
+  const setUserMaze = (maze) => {
+    setUsrMaze(maze);
+    const options = ['O', "X"];
+    setSystemMaze(options.filter(c=>c !== maze)[0])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+     <div className="row pt-2">
+     <div className="col-md-6">
+      <div className="card">
+        <div className="card-header d-flex justify-content-between">
+           <div className="d-flex">
+            <label className="me-2">Levels</label>
+            <MazeOptions setMazeSize={setMazeSize}/>
+           </div>
+           <div className="d-flex">
+            <label className="me-2">Choose your maze</label>
+            <UserMaze setUserMaze={setUserMaze}/>
+           </div>
+        </div>
+        <div className="card-body">
+          {
+            size != 0 ? <Board size="3" size={size} userMaze={userMaze} systemMaze={systemMaze}/>: <div>No Maze Selected</div>
+          }
+        </div>
+      </div>
+      </div>
+     </div>
     </div>
   );
+}
+
+const MazeOptions = ({setMazeSize}) => {
+
+  const options = {
+    0: 'Select Maze',
+    3: '3X3',
+    // 4: '4X4',
+    // 5: '5X5'
+  };
+
+  return (
+   <>
+   <select className="form-select" onChange={(e)=>setMazeSize(e.target.value)}>
+      {
+        Object.keys(options).map((key, index)=>(
+          <option value={key} key={index}>{options[key]}</option>
+        ))
+      }
+    </select>
+    </>
+  )
+}
+
+const UserMaze = ({setUserMaze}) => {
+  const options = ['X','O']
+  return (
+   <>
+     <select className="form-select" onChange={(e)=>setUserMaze(e.target.value)}>
+        {
+          options.map((key, index)=>(
+            <option value={key} key={index}>{key}</option>
+          ))
+        }
+      </select>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,232 @@
+import './../App.css'
+import { useState, useEffect } from 'react';
+import { comb_3_x_3, win_3_x_3 } from './config';
+
+function Board({size, userMaze, systemMaze}) {
+  const row = new Array(size).fill('');
+  let interval =''
+
+  const box = row.map(c=>{
+    c = new Array(size).fill('')
+    return c;
+  })
+ 
+  const [cells, setCells] = useState(box);
+  const [systemTurn, setSystemTurn] = useState(false);
+  const [timer, setTimer] = useState(60);
+  const [over, setOver] = useState(false)
+
+  const userPlay = (id) => {
+    checkPlayedAll();
+
+    if (!over){
+      const p = id.split('-');
+      setCells(prev=>{
+        prev[p[0]][p[1]] = userMaze;
+        setSystemTurn(true)
+        return [...prev]
+      });
+
+      let isWin = false;
+      
+      setTimeout(()=>{
+        isWin = winCheck(userMaze)
+        if(isWin) {
+          setOver(true)
+          alert('Congrats! You won.')
+        }
+      },500)
+      
+      setTimeout(()=>{
+        if(!isWin){
+          systemPlay(id)
+        }
+        
+      },1000)
+    }
+  }
+
+  const systemPlay = (userPlayId) => {
+   checkPlayedAll();
+
+   if(!over){
+     let systemPlayed = false;
+     let systemWinProb = checkWinComb(systemMaze);
+     let userwinProb = checkWinComb(userMaze);
+     
+
+      if (systemWinProb) {
+        let d = systemWinProb.split('-')
+        setCells(prev=>{
+            prev[d[0]][d[1]] = systemMaze;
+            return [...prev]
+        });
+
+        setTimeout(()=>{
+          setOver(true)
+          alert('System Won')
+        },500)
+        
+      }
+      else if (userwinProb) {
+         let d = userwinProb.split('-')
+         setCells(prev=>{
+            prev[d[0]][d[1]] = systemMaze;
+            return [...prev]
+          })
+      } else {
+         comb_3_x_3[userPlayId].forEach(d=>{
+           if(cells[d[0]][d[1]] == '' && !systemPlayed) {
+
+            setCells(prev=>{
+              prev[d[0]][d[1]] = systemMaze;
+              return [...prev]
+            });
+
+            systemPlayed = true;
+          }
+       })
+      }
+     setSystemTurn(false)
+    }
+  }
+
+  const checkWinComb = (maze) => {
+    let played = getAllPlays(maze);
+    let out = winProbabily(played);
+    let max = moreWinProbableIndex(out);
+    let win = preventWin(max, played);
+    return win;
+  }
+
+  const getAllPlays = (maze) => {
+    const played = [];
+    cells.forEach((row,rowInx) => {
+      row.forEach((col, colIndx) => {
+        if (cells[rowInx][colIndx] === maze) {
+          played.push(rowInx+'-'+colIndx)
+        }
+      });
+    });
+    return played;
+  }
+
+  const winProbabily = (palyed) => {
+     let out = [];
+     palyed.forEach(user => {
+       win_3_x_3.map((each, idx) => {
+         each.forEach(d => {
+            if (user === d) {
+             out.push(idx)
+            }
+         });
+       })
+    });
+    return out;
+  }
+
+  const moreWinProbableIndex = (out) => {
+    let max = [];
+    out.map(c => {
+      if (out.filter(d=>d === c).length > 1) {
+        max.push(c)
+      }
+    });
+    return max;
+  }
+
+  const preventWin = (max, palyed) => {
+     let win = []
+     if (max.length) {
+     max.map(d=> {
+       win_3_x_3[d].map(c => {
+        if (palyed.indexOf(c) === -1 && !checkAlreadyPlayed(c)) {
+          win.push(c)
+        }
+      })
+     })
+    }
+    return win[0];
+  }
+
+  const checkAlreadyPlayed = (cell) => {
+    const d = cell.split('-')
+    if(cells[d[0]][d[1]]){
+      return true
+    }
+    return false;
+  }
+
+  const checkPlayedAll = () => {
+    let playedAll = true;
+    cells.forEach((row,rowInx) => {
+      row.forEach((col, colIndx) => {
+        if (cells[rowInx][colIndx] === '') {
+          playedAll = false;
+        }
+      });
+    });
+    if(playedAll){
+      setOver(true);
+      alert('Draw Match')
+    }
+  }
+  
+  const reMatch = () => {
+    setOver(false);
+     cells.forEach((row,rowInx) => {
+      row.forEach((col, colIndx) => {
+        cells[rowInx][colIndx] = ''
+      });
+    });
+  }
+
+  const winCheck = (maze) => {
+    let win = false
+    const played = getAllPlays(maze)
+    win_3_x_3.map(each=> {
+     if(!win){
+        let p = 0
+        played.map(c=>{
+          if(each.indexOf(c)>-1){
+             p+=1;
+          }
+        });
+
+        if(p === 3){
+          win = true;
+        }
+     }
+    });
+    return win;
+  } 
+
+  return (
+    <>
+      {
+        over ? <h4>Game over <button className="btn btn-primary" onClick={reMatch}>Remach</button></h4>:<h4>{ systemTurn  ? 'Computer Turn': 'Your Turn'}</h4>
+      }
+      {
+        cells.map((each,rowInx)=>(
+          <div className={(systemTurn || over)? 'd-flex disabled':'d-flex'} >
+           {
+            each.map((c, colInx) => (
+              <Cell key={rowInx+'-'+colInx} id={rowInx+'-'+colInx} value={c} userPlay={userPlay}></Cell>
+            ))
+           }
+          </div>
+        ))
+      }
+    </>
+  );
+}
+
+const Cell = ({id, value, userPlay}) => {
+  return(
+   <div className={ value != '' ? 'border border-primary cell text-center disabled' :'border border-primary cell text-center' } onClick={()=>userPlay(id)}>
+    {value}
+   </div>
+  )
+}
+
+export default Board;
